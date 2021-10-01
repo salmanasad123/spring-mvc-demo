7aa3aaff5e6af7428ac99ac9dcb99d1ea8aa1739
salmanasad123 <slman.me@gmail.com>
2021-10-01 18:16:15 +0500

    Using @RequestParam to read form data

--- src/main/java/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/main/java/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.luv2code.springdemo.mvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -35,6 +36,23 @@ public class HelloWorldController {
 
         // create the message
         String result = "Yo! " + theName;
+
+        // add the data to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";  // return name of the view
+    }
+
+    // reading html form field named studentName and setting/binding its value to variable theName using
+    // @RequestParam attribute
+    @RequestMapping("/processFormVersionThree")
+    public String processFormVersionThree(@RequestParam("studentName") String theName, Model model) {
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Hey my friend " + theName;
 
         // add the data to the model
         model.addAttribute("message", result);
